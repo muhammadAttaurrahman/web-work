2ad100ebb66ae1d5f2399ddd27f2e75e3775bcb4
Muhammad Atta ur Rahman <53517520+muhammadAttaurrahman@users.noreply.github.com>
2023-06-05 05:30:53 -0700

    Data Base Connectivity

---
+++ insertion.php
@@ -0,0 +1,47 @@
+<?php
+
+
+phpinfo();
+
+// $host = "localhost";
+// $dbusername = "root";
+// $dbpassword = "";
+// $dbname = "NCC";
+// // Create connection
+// $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+// //data
+
+
+// $uname = filter_input(INPUT_POST,'uName');
+// $email = filter_input(INPUT_POST,'email');
+// $password = filter_input(INPUT_POST,'pwd');
+// $gender = filter_input(INPUT_POST,'gender');
+// $dob = filter_input(INPUT_POST,'dob');
+// $phone = filter_input(INPUT_POST,'phone');
+// $address = filter_input(INPUT_POST,'address');
+
+// echo($uname,$password);
+
+
+// if (mysqli_connect_error())
+// {
+// die('Connect Error ('. mysqli_connect_errno() .') '
+// . mysqli_connect_error());
+// }
+// else
+// {
+// $sql = "INSERT INTO 'nccdatabase' (Name,Email,Password,gender,dob,phone,address) 
+// VALUES ('$uname','$email','$password','$gender','$dob','$phone','$address') ";
+
+// if ($conn->query($sql))
+// {
+// echo "New record is inserted sucessfully";
+// }
+
+// $conn->close();
+// }
+
+
+
+?>
